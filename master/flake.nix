12982c2d67657ef4074bd707404c48360db988ee
hugosenari <hugosenari@gmail.com>
2022-03-22 01:15:25 -0300

    chore: re-index ../nimpkgs/p/pwd

--- master/flake.nix
+++ master/flake.nix
@@ -8,8 +8,8 @@
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
   inputs.src-pwd-master.flake = false;
+  inputs.src-pwd-master.ref   = "refs/heads/master";
   inputs.src-pwd-master.owner = "achesak";
-  inputs.src-pwd-master.ref   = "master";
   inputs.src-pwd-master.repo  = "nim-pwd";
   inputs.src-pwd-master.type  = "github";
   
